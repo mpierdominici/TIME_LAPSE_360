108760348a16710d2fb288be6a78a6a02cdf20db
JuaniBrunero <juanibrunero@gmail.com>
2017-04-12 10:04:56 -0300

    Cambio de directorios

--- Steper/Steper.ino
+++ Steper/Steper.ino
@@ -1,12 +1,18 @@
 #include "Steper.h"
+#include <SoftwareSerial.h>
 
-MStep motor(2, 3, 4, 5);
+
+MStep motor(4, 5, 6, 7);
+SoftwareSerial mySerial(10, 11);
 
 void setup(){
-  
+  Serial.begin(115200);
+  mySerial.begin(115200);
 }
 
 void loop(){
-  
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
 }
 
